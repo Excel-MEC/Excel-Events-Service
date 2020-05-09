2faef47ea6849d13c5904659aad21f13d3711e51
AjeshKumarS <ajeshkumar36020@gmail.com>
2020-05-09 11:45:24 +0530

    [Feature] Added Rules and Format

---
+++ Excel-Events-Backend/API/Migrations/20200509060033_AddedFormatAndRulesInEventsTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddedFormatAndRulesInEventsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Format",
+                table: "Events",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rules",
+                table: "Events",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Format",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Rules",
+                table: "Events");
+        }
+    }
+}
